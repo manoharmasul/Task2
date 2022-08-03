64b5d8d78b6146a2a526814454f8a442a7eb85f6
DELL <DELL@DESKTOP-6UO4B1H>
2022-08-03 03:30:51 -0700

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task2/Task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Task2
+{
+    interface IInt1
+    {
+         int add(int x,int y);
+          int mult(int x,int y);
+    }
+    class Ch:IInt1
+    {
+        public int add(int x,int y)
+        {
+            return (x + y);
+        }
+        public int mult(int x,int y)
+        {
+            return (x * y);
+        }
+    }
+
+    class Task
+    {
+        public static void Main()
+        {
+            try
+            {
+                List<int> li = new List<int>();
+
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                Ch obj = new Ch();
+                li.Add(obj.add(x, y));
+                li.Add(obj.mult(x, y));
+                FileStream file = new FileStream(@"C:\Users\DELL\Documents\Shaurya\Text22", FileMode.Create, FileAccess.Write);
+                XmlSerializer xml = new XmlSerializer(typeof(List<int>));
+                xml.Serialize(file, li);
+                Console.WriteLine("file is Created ");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
